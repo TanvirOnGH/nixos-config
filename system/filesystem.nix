ad93ad83bb6bd972024b0159ae675c7cfa50bf7b
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-08-22 00:05:01 +0600

    update hdd device uuid

--- system/filesystem.nix
+++ system/filesystem.nix
@@ -78,7 +78,7 @@
     };
 
     "/mnt/HDD" = {
-      device = "/dev/disk/by-uuid/e55cdfed-5ef8-4660-a092-389c38d0f2cf";
+      device = "/dev/disk/by-uuid/d79864d5-c286-4f5c-9005-14de43821fa7";
       fsType = "btrfs";
       options = ["compress=zstd:4"];
     };
