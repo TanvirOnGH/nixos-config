5d1e39afb9f68e9c8815a160406d7183df9307dd
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-09-13 01:47:31 +0600

    disable exa and add eza package

--- users/user/home-manager/packages.nix
+++ users/user/home-manager/packages.nix
@@ -147,7 +147,8 @@ in {
 
       # Cli tools
       bat # cli: A cat(1) clone with syntax highlighting and Git integration
-      exa # cli: Replacement for 'ls' written in Rust
+      # exa # cli: Replacement for 'ls' written in Rust
+      eza # cli: A modern, maintained replacement for ls
       grc # cli: A generic text colouriser
       tealdeer # cli: A very fast implementation of tldr in Rust
       cheat # cli: Create and view interactive cheatsheets on the command-line
