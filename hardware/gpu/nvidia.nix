c1f28627aea882d6de47530b94233db542ce16c8
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-11-23 04:17:01 +0600

    nvidia: switch to stable and disable power management

--- hardware/gpu/nvidia.nix
+++ hardware/gpu/nvidia.nix
@@ -12,10 +12,15 @@
   boot.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "i2c-nvidia_gpu"];
 
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     modesetting.enable = true;
-    powerManagement.enable = true;
+    # Experimental, and can cause sleep/suspend to fail
+    powerManagement.enable = false;
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer)
+    powerManagement.finegrained = false;
     nvidiaSettings = true;
+    # Currently alpha-quality/buggy, so false is currently the recommended setting
+    open = false;
   };
 
   environment.sessionVariables = {
