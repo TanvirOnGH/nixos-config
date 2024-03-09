4fbda77c7cc59cdaa659a91a5615d0d979cefd21
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:23 +0600

    set bluetooth.powerOnBoot to false

--- hardware/network/bluetooth.nix
+++ hardware/network/bluetooth.nix
@@ -1,5 +1,8 @@
 # Bluetooth Configuration
 #- <https://nixos.wiki/wiki/Bluetooth>
 {...}: {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = false;
+  };
 }
