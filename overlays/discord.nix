23d8e2b44e211c173765d2a4ecdd9a51dbeeba3d
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:24 +0600

    enable vencord in discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -3,7 +3,10 @@
   nixpkgs.overlays = let
     # <https://nixos.wiki/wiki/Discord#OpenAsar>
     discordOpenASAR = self: super: {
-      discord = super.discord.override {withOpenASAR = true;};
+      discord = super.discord.override {
+        withOpenASAR = true;
+        withVencord = true;
+      };
     };
   in [discordOpenASAR];
 }
