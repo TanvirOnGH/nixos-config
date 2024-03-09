3448b772e306a05878a0dcb8fa4c54ec5381274b
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:21 +0600

    exchange schedulers: sda <-> sdb

--- system/udev-rules.nix
+++ system/udev-rules.nix
@@ -4,8 +4,8 @@
     # I/O Schedulers
     extraRules = ''
       ACTION=="add|change", KERNEL=="nvme0n1", ATTR{queue/scheduler}="kyber"
-      ACTION=="add|change", KERNEL=="sda", ATTR{queue/scheduler}="mq-deadline"
-      ACTION=="add|change", KERNEL=="sdb", ATTR{queue/scheduler}="bfq"
+      ACTION=="add|change", KERNEL=="sda", ATTR{queue/scheduler}="bfq"
+      ACTION=="add|change", KERNEL=="sdb", ATTR{queue/scheduler}="mq-deadline"
     '';
   };
 }
