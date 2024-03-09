ce9271c471e92cee8e517c146eca619ef0c61f6f
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:23 +0600

    set systemd-boot configuration limit to 6

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -6,7 +6,7 @@
     systemd-boot = {
       enable = true;
       # https://nixos.wiki/wiki/Bootloader#Limiting_amount_of_entries_with_grub
-      configurationLimit = 20;
+      configurationLimit = 6;
     };
     efi.canTouchEfiVariables = true;
   };
