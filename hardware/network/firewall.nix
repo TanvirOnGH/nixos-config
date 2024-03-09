1c2058c3c5087ef94c135b771716e9e31bb62ab9
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:17 +0600

    enable k3s service

--- hardware/network/firewall.nix
+++ hardware/network/firewall.nix
@@ -10,7 +10,8 @@
       "enp42s0" = {
         # allowedTCPPorts = [80 443];
         # Syncthing ports
-        allowedTCPPorts = [8384 22000];
+        # 6433 is equired by k3s so that pod can reach the API server (running on port 6443 by default)
+        allowedTCPPorts = [6443 8384 22000];
         allowedUDPPorts = [22000 21027];
 
         allowedTCPPortRanges = [
@@ -22,7 +23,7 @@
         ];
 
         allowedUDPPortRanges = [
-          # GS-/KDE-Connect
+          # {GS/KDE}-Connect
           {
             from = 1714;
             to = 1764;
