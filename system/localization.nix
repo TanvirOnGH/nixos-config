e2c47ed45a147fbe53cc0a87d8894701f8733672
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:19 +0600

    i18n: add US UTF-8 to supported locales

--- system/localization.nix
+++ system/localization.nix
@@ -1,5 +1,8 @@
 # System-Specific Configuration
 {...}: {
   time.timeZone = "Asia/Dhaka";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = ["en_US.UTF-8/UTF-8"];
+  };
 }
