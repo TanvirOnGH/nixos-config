1d0163b7da81fc0f663d602aed560a5b9709082d
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 20:57:17 +0600

    enable waydroid

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -12,6 +12,11 @@
     virtualbox.host.enableExtensionPack = true;
     */
 
+    # <https://nixos.wiki/wiki/WayDroid>
+    # Wayland Only
+    # Running on X11 (use weston package in X11 session): <https://github.com/waydroid/waydroid/issues/195#issuecomment-953926526>
+    waydroid.enable = true;
+
     # <https://nixos.wiki/wiki/Docker>
     docker = {
       enable = true;
