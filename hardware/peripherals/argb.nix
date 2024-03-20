d42843207ab4bdb04e7ea23faef0b022acebdd22
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-20 18:27:54 +0600

    disable openrgb service

--- hardware/peripherals/argb.nix
+++ hardware/peripherals/argb.nix
@@ -2,7 +2,7 @@
 # <https://nixos.wiki/wiki/OpenRGB>
 {pkgs, ...}: {
   services.hardware.openrgb = {
-    enable = true;
+    enable = false;
     motherboard = "amd";
     server.port = 5700;
     package = pkgs.openrgb-with-all-plugins;
