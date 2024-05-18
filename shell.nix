e02ba3cb78485c122eedd83f43ab0253e0b66229
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-05-18 23:15:07 +0600

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{pkgs ? import ./nixpkgs.nix {}}: let
+  # It's generally recommended to pin your nixpkgs for reproducibility.
+  # You can use `nix flake lock` to update it.
+  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz) {};
+in {
+  default = pkgs.mkShell {
+    # Use `nix.extraOptions` instead of directly setting `NIX_CONFIG`.
+    # This is more idiomatic and future-proof.
+    nix.extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
+    buildInputs = with pkgs; [
+      nix
+      home-manager
+      git
+    ];
+  };
+}
