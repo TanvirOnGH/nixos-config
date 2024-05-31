e2434743f2d5ed62f02d71e0d66d38c93d6f2c19
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-05-31 14:56:49 +0600

    enable nvidiaPersistenced

--- hardware/gpu/nvidia.nix
+++ hardware/gpu/nvidia.nix
@@ -7,20 +7,30 @@
   ...
 }: {
   # nvidia is the proprietary driver for Nvidia GPUs
+  # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   boot.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "i2c-nvidia_gpu"];
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # Required
     modesetting.enable = true;
+
     # Experimental, and can cause sleep/suspend to fail
     powerManagement.enable = false;
+
     # Experimental and only works on modern Nvidia GPUs (Turing or newer)
     powerManagement.finegrained = false;
-    nvidiaSettings = true;
+    nvidiaSettings = true; # accessible via `nvidia-settings`
+
     # Currently alpha-quality/buggy, so false is currently the recommended setting
     open = false;
+
+    # Ensure all GPUs stay awake even during headless mode
+    # Fixes a glitch
+    nvidiaPersistenced = true;
   };
 
   environment.sessionVariables = {
