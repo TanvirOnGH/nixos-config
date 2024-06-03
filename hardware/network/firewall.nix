bb50b490e043461d14ad9548ab055446dcf6ac31
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-03 23:02:44 +0600

    enable firewall and block all ports

--- hardware/network/firewall.nix
+++ hardware/network/firewall.nix
@@ -1,19 +1,32 @@
 # Firewall Configuration
 #- <https://nixos.wiki/wiki/Firewall>
 {...}: {
-  services.opensnitch.enable = false;
+  # The default firewall uses iptables
+  # To use the newer nftables instead it needs to be enabled
+  # Causes problem with Docker
+  # Docker uses iptables internally to setup NAT for containers
+  networking.nftables.enable = false;
 
+  # Check the rules: iptables -L -v -n
+  # nftables: nft list ruleset
   networking.firewall = {
-    enable = false;
+    enable = true;
     allowPing = true;
+
+    # Warning: Firewall rules may be overwritten by Docker
+    # <https://github.com/NixOS/nixpkgs/issues/111852>
     interfaces = {
       "enp42s0" = {
-        # allowedTCPPorts = [80 443];
+        # Block all ports
+        allowedTCPPorts = [];
+        allowedUDPPorts = [];
+
         # Syncthing ports
         # 6433 is equired by k3s so that pod can reach the API server (running on port 6443 by default)
-        allowedTCPPorts = [6443 8384 22000];
-        allowedUDPPorts = [22000 21027];
+        # allowedTCPPorts = [6443 8384 22000];
+        # allowedUDPPorts = [22000 21027];
 
+        /*
         allowedTCPPortRanges = [
           # GS-/KDE-Connect
           {
@@ -29,6 +42,7 @@
             to = 1764;
           }
         ];
+        */
       };
     };
   };
