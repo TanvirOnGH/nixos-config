ba5923a0abf493c245018949b0418ed649ceed18
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-04 12:24:07 +0600

    comment-out hyprland cache

--- system/nix/cache.nix
+++ system/nix/cache.nix
@@ -7,12 +7,12 @@
     substituters = [
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
-      "https://hyprland.cachix.org"
+      # "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 }
