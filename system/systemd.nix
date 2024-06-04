cdbff9c67075e9a6db1cc83052d170e76e550522
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-04 12:03:25 +0600

    disable systemd coredump

---
+++ system/systemd.nix
@@ -0,0 +1,7 @@
+# Systemd Configuration
+{...}: {
+  # Disable coredump that could be exploited later
+  # and also slow down the system when something crash
+  # If disabled, core dumps appear in the current directory of the crashing process
+  systemd.coredump.enable = false;
+}
