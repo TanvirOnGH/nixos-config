b1e7d757b3e0fea66388f6e16b7891fb9f219be5
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-10 04:44:40 +0600

    shell: switch to v24.05

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 {pkgs ? import ./nixpkgs.nix {}}: let
   # It's generally recommended to pin your nixpkgs for reproducibility.
   # You can use `nix flake lock` to update it.
-  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz) {};
+  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz) {};
 in {
   default = pkgs.mkShell {
     # Use `nix.extraOptions` instead of directly setting `NIX_CONFIG`.
