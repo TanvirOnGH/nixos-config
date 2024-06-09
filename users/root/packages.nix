c87f84b0b3f8fa26af7a0ced201396438e98e2d4
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-10 00:13:25 +0600

    enable autopair and puffer fish plugin globally

--- users/root/packages.nix
+++ users/root/packages.nix
@@ -3,5 +3,15 @@
   environment.systemPackages = with pkgs; [
     # USER.hblock # cli: shell script for blocking ads, malware and tracking domains
     gparted # gui: partition manager for gnome
+
+    # <https://nixos.wiki/wiki/Fish#System_wide>
+    # fishPlugins.wakatime-fish # plugin: wakatime fish plugin
+    # fishPlugins.colored-man-pages # plugin: Fish shell plugin to colorize man pages
+    # fishPlugins.forgit # plugin: A utility tool powered by fzf for using git interactively
+    fishPlugins.autopair # plugin: Auto-complete matching pairs in the Fish command line
+    # fishPlugins.plugin-git # plugin: Git plugin for fish (similar to oh-my-zsh git)
+    fishPlugins.puffer # plugin: Text Expansions for Fish
+
+    
   ];
 }
