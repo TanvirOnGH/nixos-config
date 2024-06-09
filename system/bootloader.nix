8e43e352abfe9d04e471d8547ca73b1fd0b5bf9a
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-10 05:09:38 +0600

    disable system-boot editor

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -7,6 +7,13 @@
       enable = true;
       # https://nixos.wiki/wiki/Bootloader#Limiting_amount_of_entries_with_grub
       configurationLimit = 6;
+
+      # Allow editing the kernel command-line before boot
+      # enabled by default for backwards compatibility
+      # Recommended to set this to false,
+      # as it allows gaining root access by passing
+      # init=/bin/sh as a kernel parameter
+      editor = false;
     };
     efi.canTouchEfiVariables = true;
   };
