69ca0d2dff5f766834bede4b9bee50e86e229d48
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-10 21:47:58 +0600

    rename discordOpenASAR overlay to discord_OpenASAR_Vencord for clarity

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,12 +1,12 @@
 # <https://nixos.wiki/wiki/Discord>
 {...}: {
   nixpkgs.overlays = let
-    # <https://nixos.wiki/wiki/Discord#OpenAsar>
-    discordOpenASAR = self: super: {
+    # <https://nixos.wiki/wiki/Discord#OpenAsar_.2F_Vencord>
+    discord_OpenASAR_Vencord = self: super: {
       discord = super.discord.override {
         withOpenASAR = true;
         withVencord = true;
       };
     };
-  in [discordOpenASAR];
+  in [discord_OpenASAR_Vencord];
 }
