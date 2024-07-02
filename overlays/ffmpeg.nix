b448059bf9b486034fffb1f02cdaf2e8cbe23a45
TanvirOnGH <TanvirOnGH@users.noreply.github.com>
2024-07-02 01:03:39 +0000

    chore: fix dead code with deadnix

--- overlays/ffmpeg.nix
+++ overlays/ffmpeg.nix
@@ -1,6 +1,6 @@
 _: {
   nixpkgs.overlays = [
-    (self: super: {
+    (_self: super: {
       # <https://nixos.wiki/wiki/CCache#Derivation_CCache_2>
       ffmpeg = super.ffmpeg.override {stdenv = super.ccacheStdenv;};
     })
