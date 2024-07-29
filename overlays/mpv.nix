6158b72947bb8a81e4949daf9e5c49ad0acc058c
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-07-29 09:10:32 +0600

    add youtube-upnext mpv userscript to mpv overlay and improve other userscripts' comment-descriptions

--- overlays/mpv.nix
+++ overlays/mpv.nix
@@ -4,16 +4,17 @@ _: {
     (self: super: {
       mpv = super.mpv.override {
         scripts = with self.mpvScripts; [
-          uosc # Feature-rich minimalist proximity-based UI for MPV player
-          thumbfast # High-performance on-the-fly thumbnailer for mpv
-          seekTo # Mpv script for seeking to a specific position
-          webtorrent-mpv-hook # plugin: Adds a hook that allows mpv to stream torrents
-          mpv-cheatsheet # mpv script for looking up keyboard shortcuts
-          quality-menu # A userscript for MPV that allows you to change youtube video quality (ytdl-format) on the fly
-          sponsorblock # Script for mpv to skip sponsored segments of YouTube videos
-          reload # Manual & automatic reloading of videos
-          autoload # This script automatically loads playlist entries before and after the currently played file
-          autocrop # This script uses the lavfi cropdetect filter to automatically insert a crop filter with appropriate parameters for the currently playing video
+          uosc # Userscript: Feature-rich minimalist proximity-based UI
+          thumbfast # Userscript: High-performance on-the-fly thumbnailer
+          seekTo # Userscript: For seeking to a specific position
+          webtorrent-mpv-hook # Userscript: Adds a hook that allows mpv to stream torrents
+          mpv-cheatsheet # Userscript: For looking up keyboard shortcuts
+          quality-menu # Userscript: Allows you to change youtube video quality (ytdl-format) on the fly
+          sponsorblock # Userscript: Skip sponsored segments of YouTube videos
+          reload # Userscript: Manual & automatic reloading of videos
+          autoload # Userscript: Automatically loads playlist entries before and after the currently played file
+          autocrop # Userscript: Uses the lavfi cropdetect filter to automatically insert a crop filter with appropriate parameters for the currently playing video
+          youtube-upnext # Userscript: Allows you to play 'up next'/recommended youtube videos
         ];
       };
     })
