dfedaac8efd93aec40d44eff9c8a66c26fdfc94d
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-02 13:19:10 +0600

    remove nvidia specific environment session variables

--- hardware/gpu/nvidia.nix
+++ hardware/gpu/nvidia.nix
@@ -39,19 +39,7 @@
 
       # NVidia open source kernel module (not to be confused with the
       # independent third-party "nouveau" open source driver)
-      # <https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus>
-      # A Look at their new releases
-      # <https://us.download.nvidia.com/XFree86/Linux-x86_64/550.54.14/README/kernel_open.html>
-      # says "Use of the open kernel modules on GeForce and Workstation GPUs should be considered Beta quality ..."
-      # In nixos official wiki it says:
-      # "Currently "beta quality", so false is currently the recommended setting."
-      # "Nvidia's open-source kernel module, which is beta-quality and x11-only."
-      # <https://wiki.nixos.org/wiki/Nvidia#Modifying_NixOS_configuration>
-      # 23/7/2024: NVIDIA 560 (560.28.03) Linux Driver Beta Released - Defaults To Open GPU Kernel Modules
-      # <https://www.phoronix.com/news/NVIDIA-560.28.03-Beta-Linux>
-      # TODO: Investigate the current state of this driver and the practicality of using it
-      # open = lib.mkForce true;
-      open = true; # for testing
+      open = true;
 
       # Ensure all GPUs stay awake even during headless mode
       # Fixes a glitch
@@ -66,11 +54,13 @@
     };
   };
 
+  /*
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
     VDPAU_DRIVER = "va_gl";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
+  */
 
   hardware.graphics.extraPackages = with pkgs; [
     vaapiVdpau
