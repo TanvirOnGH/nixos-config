0ea7e07d318eb04c87b028312048aab79af3e252
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-02 13:48:46 +0600

    remove all packages from permitted insecure packages

--- temporary.nix
+++ temporary.nix
@@ -1,8 +1,10 @@
 _: {
   nixpkgs.config.permittedInsecurePackages = [
-    # "python3.12-youtube-dl-2021.12.17"
+    /*
+    "python3.12-youtube-dl-2021.12.17"
     "jitsi-meet-1.0.8043"
     "fluffychat-linux-1.20.0"
     "olm-3.2.16"
+    */
   ];
 }
