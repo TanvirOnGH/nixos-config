9d663ceec10dcd5f4fd79d72f770f0bce77487e5
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-15 09:03:06 +0600

    hardened kernel config: update kernel sysctl configurations

--- hardened/kernel.nix
+++ hardened/kernel.nix
@@ -1,11 +1,3 @@
-# Hardened Kernel Configuration
-#- Based on <https://github.com/mbund/nix-config/blob/main/hardware/hardened-server-kernel.nix>
-# More at:
-#- <https://privsec.dev/posts/linux/desktop-linux-hardening>
-#- <https://madaidans-insecurities.github.io/guides/linux-hardening.html>
-#- <https://wiki.archlinux.org/title/Kernel_parameters>
-#- <https://github.com/Kicksecure/security-misc/tree/master/etc/sysctl.d>
-#- <https://systemd.io/RANDOM_SEEDS>
 {
   pkgs,
   lib,
@@ -18,6 +10,12 @@
     kernel.sysctl = {
       # Disable magic SysRq key
       "kernel.sysrq" = 0;
+      # Disable NMI watchdog
+      "kernel.nmi_watchdog" = 0;
+      # To hide any kernel messages from the console
+      "kernel.printk" = "3 3 3 3";
+      # Only swap when absolutely necessary
+      "vm.swappiness" = 10;
       # Ignore ICMP broadcasts to avoid participating in Smurf attacks
       "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
       # Ignore bad ICMP errors
@@ -36,6 +34,7 @@
       "net.ipv6.conf.default.accept_redirects" = 0;
       # Do not send ICMP redirects (we are not a router)
       "net.ipv4.conf.all.send_redirects" = 0;
+      "net.ipv4.conf.default.send_redirects" = 0;
       # Do not accept IP source route packets (we are not a router)
       "net.ipv4.conf.all.accept_source_route" = 0;
       "net.ipv6.conf.all.accept_source_route" = 0;
@@ -43,11 +42,19 @@
       "net.ipv4.tcp_rfc1337" = 1;
       # Latency reduction
       "net.ipv4.tcp_fastopen" = 3;
-      ## Bufferfloat mitigations
+      # Bufferfloat mitigations
       # Requires >= 4.9 & kernel module
       "net.ipv4.tcp_congestion_control" = "bbr";
       # Requires >= 4.19
       "net.core.default_qdisc" = "cake";
+      # Increase the TCP receive and transmit buffer sizes
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_max" = 16777216;
+      # Enable TCP window scaling and timestamps
+      "net.ipv4.tcp_window_scaling" = 1;
+      "net.ipv4.tcp_timestamps" = 1;
+      # Increase the maximum number of open file descriptors
+      "fs.file-max" = 65535;
     };
 
     blacklistedKernelModules = [
