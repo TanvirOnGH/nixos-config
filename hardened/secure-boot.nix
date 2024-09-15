4b56153c152da9b38dd838c516d0642e5ee7a2c2
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-15 09:37:11 +0600

    hardened kernel config: init secure boot config

--- hardened/secure-boot.nix
+++ hardened/secure-boot.nix
@@ -1,5 +1,18 @@
 # Secure Boot Configuration
 # <https://wiki.nixos.org/wiki/Secure_Boot>
 # TODO: lanzaboote <https://github.com/nix-community/lanzaboote>
-_: {
+{
+  lib,
+  config,
+  ...
+}: {
+  # Recommended to set this to false,
+  # as it allows gaining root access by passing
+  # init=/bin/sh as a kernel parameter
+  boot.loader.systemd-boot.editor = lib.mkForce false;
+
+  boot.tmp.useTmpfs = lib.mkForce false;
+
+  # /tmp should be treated as volatile storage!
+  boot.tmp.cleanOnBoot = lib.mkForce (!config.boot.tmp.useTmpfs);
 }
